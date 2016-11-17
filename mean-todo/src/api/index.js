283c17a804b931c5aa99468de70c214b07204557
Anija <anija.nair@yahoo.com>
2016-11-17 14:26:58 -0500

    replaced mock data with Todo model

--- mean-todo/src/api/index.js
+++ mean-todo/src/api/index.js
@@ -3,13 +3,24 @@
 'use strict';
 
 var express = require('express');
-var todos = require('../../mock/todos.json');
+var Todo = require('../models/todo');
+
+/* Commenting out the mock data as there is Todo model now*/
+//var todos = require('../../mock/todos.json');
 
 //setting up route for API and mounting it on router
 var router = express.Router();
 
 router.get('/todos', function (req, res) {
-  res.json({todos:todos});
+
+  //getting todos
+  Todo.find({}, function (err, todos) {
+    if (err) {
+      return res.status(500).json({message: err.message});
+    }
+    res.json({todos:todos});
+  });
+
 });
 
 // Adding POST route to create new entries
